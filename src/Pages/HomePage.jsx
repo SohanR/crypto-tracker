ad5a08602827a6766a4a19158f33196b493215a3
sohanr <mizan.rahman66d@gmail.com>
2022-03-01 17:59:37 +0600

    homepage

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../components/Banner/Banner'
+
+const HomePage = () => {
+  return (
+    <>
+      <Banner/>
+    </>
+  )
+}
+
+export default HomePage
