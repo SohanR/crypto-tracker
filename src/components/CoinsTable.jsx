0a5930cd9dce56e4055dfe0f75060dd01348cf7c
sohanr <mizan.rahman66d@gmail.com>
2022-03-07 22:42:06 +0600

    search coin

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -1,4 +1,5 @@
 
+import { Container, createTheme, TextField, ThemeProvider, Typography } from '@material-ui/core';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { CoinList } from '../config/api';
@@ -8,6 +9,7 @@ const CoinsTable = () => {
 
     const [coins, setCoins] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState();
 
 
 
@@ -29,8 +31,35 @@ const CoinsTable = () => {
        fetchCoins();
     }, [currency]);
 
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main:"#fff"    
+            },
+            type:"dark"
+        }
+    })
+
   return (
-    <div>CoinsTable</div>
+    <ThemeProvider theme={darkTheme} >
+        <Container style={{textAlign:"center"}} >
+            <Typography
+                variant="h4"
+                style={{margin:18, fontFamily:"Montserrat"}}
+            >
+                Cryptocurrency Prices by Market Cap
+            </Typography>
+
+            <TextField 
+                label="Search for a Crypto Currency..."
+                variant='outlined'
+                style={{marginBottom:20, width:"100%"}}
+                onChange={e => setSearch(e.target.value)}
+            
+            />
+        </Container>
+
+    </ThemeProvider>
   )
 }
 
